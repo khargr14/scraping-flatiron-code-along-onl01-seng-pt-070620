e3af9b6f6269b9b06e7116bcc265e8e0f8f4f3de
Keesha Hargrove <kharg14@gmail.com>
2020-08-13 09:31:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,6 +17,13 @@ class Scraper
     end
   end
   
+  
+  def get_page
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      binding.pry
+  end
+ 
 end
 
 
